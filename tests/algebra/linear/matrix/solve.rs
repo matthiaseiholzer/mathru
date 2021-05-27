0542c2d230237cbbe50a6379756bb86d30764aab
Matthias Eiholzer <matthias.eiholzer@gmail.com>
2021-05-27 20:53:29 +0200

    Add solver for system of linear equeations with complex numbers

--- tests/algebra/linear/matrix/solve.rs
+++ tests/algebra/linear/matrix/solve.rs
@@ -38,6 +38,41 @@ fn solve_matrix_f64()
 }
 
 #[test]
+fn solve_matrix_complex_f32()
+{
+    let a: Matrix<Complex<f32>> = matrix![  Complex::new(1.0, 0.0), Complex::new(-2.0, 0.0), Complex::new(3.0, 0.0);
+                                            Complex::new(2.0, 0.0), Complex::new(-5.0, 0.0), Complex::new(12.0, 0.0);
+                                            Complex::new(0.0, 0.0), Complex::new(2.0, 0.0), Complex::new(-10.0, 0.0)];
+
+    let x_ref: Matrix<Complex<f32>> = matrix![  Complex::new(-13.0, 0.0), Complex::new(7.0, 0.0), Complex::new(4.5, 0.0);
+                                                Complex::new(-10.0, 0.0), Complex::new(5.0, 0.0), Complex::new(3.0, 0.0);
+                                                Complex::new(-2.0, 0.0), Complex::new(1.0, 0.0), Complex::new(0.5, 0.0)];
+
+    let id: Matrix<Complex<f32>> = Matrix::one(3);
+
+    let x: Matrix<Complex<f32>> = a.solve(&id).unwrap();
+
+    assert_relative_eq!(x, x_ref);
+}
+
+#[test]
+fn solve_matrix_complex_f64()
+{
+    let a: Matrix<Complex<f64>> = matrix![  Complex::new(1.0, 0.0), Complex::new(-2.0, 0.0), Complex::new(3.0, 0.0);
+                                            Complex::new(2.0, 0.0), Complex::new(-5.0, 0.0), Complex::new(12.0, 0.0);
+                                            Complex::new(0.0, 0.0), Complex::new(2.0, 0.0), Complex::new(-10.0, 0.0)];
+
+    let x_ref: Matrix<Complex<f64>> = matrix![  Complex::new(-13.0, 0.0), Complex::new(7.0, 0.0), Complex::new(4.5, 0.0);
+                                                Complex::new(-10.0, 0.0), Complex::new(5.0, 0.0), Complex::new(3.0, 0.0);
+                                                Complex::new(-2.0, 0.0), Complex::new(1.0, 0.0), Complex::new(0.5, 0.0)];
+    let id: Matrix<Complex<f64>> = Matrix::one(3);
+
+    let x: Matrix<Complex<f64>> = a.solve(&id).unwrap();
+
+    assert_relative_eq!(x, x_ref);
+}
+
+#[test]
 fn solve_vector_f32()
 {
     let a: Matrix<f32> = matrix![   6.0, 2.0, -1.0;
